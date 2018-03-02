3d2ce3a6be7c5db7926a8403cbab5c99ccb43b9b
Luka Kobalia <lkobal2013@gmail.com>
2018-03-03 01:16:20 +0400

    First Commit - Draft Template

---
+++ Node/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/login', function(req, res){
+    console.log(req.body.username + " " +  req.body.password);
+    if (req.body.username === "brian" && req.body.password === "pass") {
+        res.json(200, {status:"success"});
+    }
+    else {
+        res.json(401, {status:"failure"})
+    }
+});
+
+var server = app.listen(8080);
